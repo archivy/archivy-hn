305095e7e997c195be9cd56fe2e4594191b6da5c
Uzay-G <uzgirit@gmail.com>
2020-11-16 23:57:55 +0100

    fix code style and trivial bugs

--- archivy_hn/__init__.py
+++ archivy_hn/__init__.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from getpass import getpass
-import re
 import sys
 
 import click
@@ -12,15 +11,22 @@ from bs4 import BeautifulSoup
 
 BASE_URL = "https://news.ycombinator.com"
 
+
 def build_comments(comment, tabs):
-    cur = "\n\n" + "\t" * tabs + f"- {comment['text']} by [{comment['author']}](https://news.ycombinator.com/user?id={comment['author']})"
+    cur = "\n\n" + "\t" * tabs + (
+            f"- {comment['text']} by "
+            "[{comment['author']}](https://news.ycombinator.com/user?id={comment['author']})"
+          )
 
     for child in comment["children"]:
         cur += build_comments(child, tabs + 1)
     return cur
 
+
 @click.command()
-@click.option("--save_comments", is_flag=True, help="Whether or not the hacker news comments should also be saved.")
+@click.option("--save_comments",
+              is_flag=True,
+              help="Whether or not the hacker news comments should also be saved.")
 def hn_sync(save_comments):
     with app.app_context():
         session = requests.Session()
@@ -50,17 +56,13 @@ def hn_sync(save_comments):
             print(f"Getting results of page {i}")
             r = session.get(url + str(i), headers=headers)
 
-            tree = BeautifulSoup(r.text)
+            tree = BeautifulSoup(r.text, features="lxml")
 
-            # Part that contains the title and url for the stories
-            tree_title = tree.select(".title")
             # Part that contains metadata such as author, no. of comments, etc.
             tree_subtext = tree.select(".subtext")
 
-            tree_score = tree.select(".subtext span.score")
-
             # Number of links on the page
-            n = len(tree_score)
+            n = len(tree_subtext)
 
             if not n:
                 print(f"Processing page {i}. No links found.")
@@ -72,36 +74,37 @@ def hn_sync(save_comments):
                 # This is to take care of situations where flag link may not be
                 # present in the subtext. So number of links could be either 3
                 # or 4.
+                # get post id by looking at link to comments
                 num_subtext = len(tree_subtext_each)
                 post_id = int(tree_subtext_each[num_subtext - 1]['href'].split("=")[1])
 
                 # call algolia api
                 res = requests.get(f"https://hn.algolia.com/api/v1/items/{post_id}").json()
-                if res["type"] == "story":
-                    bookmark = DataObj(path='hacker_news/', date=datetime.utcfromtimestamp(res["created_at_i"]), type="bookmark")
+                # might return a 404 if not indexed
+                if res.get("type") and res["type"] == "story":
+                    bookmark = DataObj(path='hacker_news/',
+                                       date=datetime.utcfromtimestamp(res["created_at_i"]),
+                                       type="bookmark")
                     hn_link = f"https://news.ycombinator/item?id={post_id}"
                     if res["url"]:
-                        bookmark.url = res["url"] 
+                        bookmark.url = res["url"]
                         bookmark.process_bookmark_url()
                     else:
                         bookmark.url = hn_link
                         bookmark.content = res["title"].replace("<p>", "").replace("</p>", "")
-                        
+
                     bookmark.title = res["title"]
                     bookmark.content = f"{res['points']} points on [Hacker News]({hn_link})\n\n{bookmark.content}"
-
 
                     if save_comments:
                         for comment in res["children"]:
                             bookmark.content += "\n\n" + build_comments(comment, 0)
                     bookmark.insert()
-
-                print(f"Saving {res['title']}...")
-
-
-                links_processed += 1
+                    links_processed += 1
+                    print(f"Saving {res['title']}...")
 
             if n < 30:
+                # no more links
                 break
 
             i += 1
