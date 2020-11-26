03c45e6f0fe32fe69886882b09c08c0070eb0133
Uzay-G <uzgirit@gmail.com>
2020-11-26 21:11:09 +0100

    ADD: requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt', encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+    install_requires = [x.strip() for x in all_reqs]
+
 setup(
     name='archivy_hn',
     version='0.1.0',
@@ -18,6 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License"
     ],
     packages=find_packages(),
+    install_requires=install_requires,
     entry_points='''
         [archivy.plugins]
         hn-sync=archivy_hn:hn_sync
